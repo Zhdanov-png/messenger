5fd8e974b104d7a0036d4ec68b7548b0b3124551
git-valeriy <git.valera@bk.ru>
2019-11-30 22:39:14 +0300

    Выборка SELECT

--- web/index.php
+++ web/index.php
@@ -2,4 +2,10 @@
 
 include "pdo.php";
 
-echo "test pdo1";
+//echo "test pdo1";
+
+$stmt = $pdo->query("SELECT `from`, `to`, `text` FROM messenges");
+while ($row = $stmt->fetch())
+{
+    echo $row['from'] ." ". $row['to'] ." ".$row['text'] ."\n";
+}
